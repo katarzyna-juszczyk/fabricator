c08edf9454a719b0cc854418f3821870f1b40a29
katarzynaMW <katarzyna.juszczyk@makingwaves.com>
2016-06-13 14:04:10 +0200

    comment removed out of js

--- src/assets/toolkit/scripts/icons-preview.js
+++ src/assets/toolkit/scripts/icons-preview.js
@@ -2,18 +2,14 @@ $(function () {
     var displayIconsPreview = window.getComputedStyle(document.body, '::after');
     displayIconsPreview = displayIconsPreview.content;
 
-    console.log('ihatethis');
-    console.log(displayIconsPreview);
-    console.log((displayIconsPreview.toString()) == "true".toString());
     if (true) { // put in a proper condition
-      console.log('dupa');
         var iconsList = window.getComputedStyle(document.body, '::before');
         iconsList = iconsList.content.slice(0, -1).split(",");
         var html = '<div class="icon-preview"><ol>';
 
         for (var i = 1; i < iconsList.length; i++) {
-            html += '<li><h6>icon-' + iconsList[i] + '</h6>'
-            + '<svg class="icon-' + iconsList[i].trim() + '"">'
+            html += '<li><h6>icon-'+iconsList[i]+'</h6>'
+            + '<svg class="icon-'+iconsList[i].trim()+'"">'
             + '<use xlink:href="assets/toolkit/svgIcons/svgIcons.svg#'+iconsList[i].trim().substring(0, iconsList[i].trim().indexOf("--"))+'" />'
             + '</svg>'
             + '</li>'
