e1feb2d0a1721e2756b1d278fbb2992346c4cd62
katarzynaMW <katarzyna.juszczyk@makingwaves.com>
2016-06-13 13:44:57 +0200

    icons preview

--- src/assets/toolkit/scripts/icons-preview.js
+++ src/assets/toolkit/scripts/icons-preview.js
@@ -12,11 +12,11 @@ $(function () {
         var html = '<div class="icon-preview"><ol>';
 
         for (var i = 1; i < iconsList.length; i++) {
-            html += '<li><i class="icon-' + iconsList[i].trim() + '"> icon-' + iconsList[i]
-            + '<svg class="icon">'
+            html += '<li><h6>icon-' + iconsList[i] + '</h6>'
+            + '<svg class="icon-' + iconsList[i].trim() + '"">'
             + '<use xlink:href="assets/toolkit/svgIcons/svgIcons.svg#'+iconsList[i].trim().substring(0, iconsList[i].trim().indexOf("--"))+'" />'
             + '</svg>'
-            + '</i></li>'
+            + '</li>'
 
         }
 
